43cd12f18cb96c70782b77ea15ea9754ad139a87
rotomicora <noxius@projectnoxius.xyz>
2023-08-02 19:35:07 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # rotomicora#0001
 # https://t.me/projectnoxius
 
-import os, fade, time, requests, random, string; from colorama import Fore
+import os, fade, time, requests, random, json; from colorama import Fore
 
 W = Fore.LIGHTWHITE_EX; R = Fore.RED; G = Fore.LIGHTGREEN_EX; B = Fore.BLUE; M = Fore.LIGHTMAGENTA_EX; C = Fore.LIGHTCYAN_EX; Y = Fore.LIGHTYELLOW_EX; BLACK = Fore.LIGHTBLACK_EX; RESET = Fore.RESET
 
@@ -11,7 +11,7 @@ GUI = """
           ╠══════════════════════════════════════════════════════╣
           ║          ╔╗╔ ╔═╗ ═╗╔═ ╦ ╦ ╦ ╔═╗                      ║
           ║          ║║║ ║ ║ ╔╩╚╗ ║ ║ ║ ╚═╗  @rotomicora#0001    ║
-          ║          ╝╚╝ ╚═╝ ╩  ╩ ╩ ╚═╝ ╚═╝       v2.0           ║
+          ║          ╝╚╝ ╚═╝ ╩  ╩ ╩ ╚═╝ ╚═╝                      ║
           ║    ╔════════════════════════════════════════════╗    ║
           ║ ╔══╝       [!] NOXIUS TOKEN CHECKER [!]         ╚══╗ ║
           ╠═╝            [-] Telegram: @tcp_nx [-]             ╚═╣
@@ -20,6 +20,10 @@ GUI = """
 
 VALID_TOKENS = 0
 NO_VALID_TOKENS = 0
+LOCKED_TOCKENS = 0
+NITRO_TOKENS = 0
+BILLING_TOKENS = 0
+NO_LOCKED_TOKENS = 0
 
 FADED_GUI = fade.pinkred(GUI)
 
@@ -55,6 +59,8 @@ def cleanFiles():
         billingTokensFile.write("")
     with open('output/Locked.txt', 'w') as lockedTokensFile:
         lockedTokensFile.write("")
+    with open('output/NotLocked.txt', 'w') as notLockedTokensFile:
+        notLockedTokensFile.write("")
 
 def checkToken(token):
     global workingTokensFile, notWorkingTokensFile
@@ -62,19 +68,24 @@ def checkToken(token):
         tokenHeaders = {"authorization": token}
         if useProxys == True:
             getProxy()
-            tokenRequest = requests.get("https://discordapp.com/api/v6/users/@me", headers=tokenHeaders, proxies=allProxys)
+            tokenRequest = requests.get("https://discordapp.com/api/v6/users/@me", headers=tokenHeaders, proxies=allProxys).json()
         else:
-            tokenRequest = requests.get("https://discordapp.com/api/v6/users/@me", headers=tokenHeaders)
+            tokenRequest = requests.get("https://discordapp.com/api/v6/users/@me", headers=tokenHeaders).json()
         if tokenRequest.status_code == 200:
             tokenRequest = tokenRequest.json()
             tokenUsername = tokenRequest["username"] + "#" + tokenRequest["discriminator"]
-            tokenNitro = requests.get("https://discordapp.com/api/v6/users/@me/billing/payments", headers=tokenHeaders)
-            if tokenNitro.status_code == 200:
-                tokenNitro = True
+            tokenLocked = requests.get("https://discordapp.com/api/v6/users/@me/settings", headers=tokenHeaders)
+            if tokenLocked.status_code == 200:
+                tokenLocked = False
             else:
+                tokenLocked = True
+            tokenNitro = tokenRequest["premium_type"]
+            if tokenNitro == 0:
                 tokenNitro = False
+            elif tokenNitro == 2:
+                tokenNitro = True                    
             tokenBilling = requests.get("https://discordapp.com/api/v6/users/@me/billing/payment-sources", headers=tokenHeaders)
-            if tokenBilling.status_code == 200:
+            if len(tokenBilling.json()) != 0 and tokenLocked == False:
                 tokenBilling = True
             else:
                 tokenBilling = False
@@ -84,27 +95,35 @@ def checkToken(token):
             else:
                 tokenLocked = True
             hiddedToken = token[0:4] + "*******" + token[24:32]
-            print(f"{M}[{G}VALID{M}] {Y}{hiddedToken} {M}| {BLACK}USERNAME: {Y}{tokenUsername} {M}| {BLACK}NITRO: {Y}{tokenNitro} {M}| {BLACK}BILLING: {Y}{tokenBilling} {M}| {BLACK}LOCKED: {Y}{tokenLocked}")
+            
             with open('output/Working.txt', 'a') as workingTokensFile:
                 workingTokensFile.write(token + "\n")
-                global VALID_TOKENS
+                global VALID_TOKENS, NITRO_TOKENS, BILLING_TOKENS, LOCKED_TOCKENS, NO_LOCKED_TOKENS
                 VALID_TOKENS += 1
             if tokenNitro == True:
                 with open('output/Nitro.txt', 'a') as nitroTokensFile:
                     nitroTokensFile.write(token + "\n")
+                    NITRO_TOKENS += 1
             if tokenBilling == True:
                 with open('output/Billing.txt', 'a') as billingTokensFile:
                     billingTokensFile.write(token + "\n")
+                    BILLING_TOKENS += 1
             if tokenLocked == True:
                 with open('output/Locked.txt', 'a') as lockedTokensFile:
                     lockedTokensFile.write(token + "\n")
+                    LOCKED_TOCKENS += 1
+            if tokenLocked == False:
+                with open('output/NotLocked.txt', 'a') as notLockedTokensFile:
+                    notLockedTokensFile.write(token + "\n")
+                    NO_LOCKED_TOKENS += 1
+            print(f"{M}[{G}VALID{M}] {Y}{hiddedToken} {M}| {BLACK}USERNAME: {Y}{tokenUsername} {M}| {BLACK}NITRO: {Y}{tokenNitro} {M}| {BLACK}BILLING: {Y}{tokenBilling} {M}| {BLACK}LOCKED: {Y}{tokenLocked}")
         else:
             print(f"{M}[{W}INVALID{M}] {R}{token}")
             with open('output/NotWorking.txt', 'a') as notWorkingTokensFile:
                 notWorkingTokensFile.write(token + "\n")
                 global NO_VALID_TOKENS
                 NO_VALID_TOKENS += 1
-        os.system(f"title Noxius Token Checker ^| Valid Tokens: {VALID_TOKENS} ^| Invalid Tokens: {NO_VALID_TOKENS} ^| Checked Tokens: {VALID_TOKENS + NO_VALID_TOKENS}/{len(allTokens)}")
+        os.system(f"title Noxius Token Checker ^| Valid Tokens: {VALID_TOKENS} ^| Invalid Tokens: {NO_VALID_TOKENS} ^| Nitro: {NITRO_TOKENS} ^| Locked: {LOCKED_TOCKENS}/{len(allTokens)} ^| Checked Tokens: {VALID_TOKENS + NO_VALID_TOKENS}/{len(allTokens)}")
     except Exception as e:
         print(f"{M}[{W}ERROR{M}] {R}{e}")
 
@@ -121,7 +140,6 @@ def main():
         useProxys = False
     for token in allTokens:
         checkToken(token)
-        time.sleep(1)
     with open('output/Working.txt') as workingTokensFile:
         workingTokensFile = workingTokensFile.read().splitlines()
     with open('output/NotWorking.txt') as notWorkingTokensFile:
